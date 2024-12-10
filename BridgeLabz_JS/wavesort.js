5f3edd4042858c49a23b4b3d47f443d828f1f4f4
sharanpreet singh <sharanpreet1831.be21@chitkara.edu.in>
2024-12-10 09:37:55 +0530

    Sharanpreet modified wavequestion

--- BridgeLabz_JS/wavesort.js
+++ BridgeLabz_JS/wavesort.js
@@ -1,10 +1,10 @@
 let arr = [ 10, 90, 49, 2, 1, 5, 23 ];
 let n = arr.length;
 
-function sort(arr,n){
+function sortarray(arr,n){
 
     arr.sort((a,b) => a -b );
-    for (var i =0 ; i<n ; i+2){
+    for (var i =0 ; i<n-1 ; i+=2){
         swap(arr,i,i+1);
     }
 }
@@ -14,5 +14,8 @@ function swap(arr, x, y)
     arr[x] = arr[y];
     arr[y] = temp
 }
+sortarray(arr,n);
 
-console.log(arr);
+for (var i = 0 ; i< n ; i++){
+    console.log(arr[i]);
+}
