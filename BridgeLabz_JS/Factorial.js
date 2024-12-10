e227b92261d303b255c2c8b9d2fbcbd0744b2e32
sharanpreet singh <sharanpreet1831.be21@chitkara.edu.in>
2024-12-10 11:00:06 +0530

    more question

---
+++ BridgeLabz_JS/Factorial.js
@@ -0,0 +1,10 @@
+let n = 50 ;
+let result = BigInt(1);
+while (n>0){
+    result = result * BigInt(n) ;
+    n = n-1 ;
+}
+console.log(result);
+
+
+
---
+++ BridgeLabz_JS/KthSmallest.js
@@ -0,0 +1,6 @@
+var arr = [7, 10, 4, 3, 20, 15];
+arr.sort((a,b)=> a-b )
+var k = 3;
+
+console.log(arr[k-1]);
+// console.log(arr[(n-k)])
